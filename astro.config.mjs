a13311d8dbaa3070581d4b1a0c837324f267666e
Iberson <iberson-silva@outlook.com>
2025-04-19 23:35:57 -0500

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import mdx from '@astrojs/mdx';
-// https://astro.build/config
 import partytown from '@astrojs/partytown'
-import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: 'https://yumm-a.netlify.app/',
-    integrations: [
-        mdx(), 
-        sitemap(),
-        partytown({
-            config: {
-              forward: ["dataLayer.push"],
-            },
-        }),
-    ],
+  integrations: [partytown()],
 });
