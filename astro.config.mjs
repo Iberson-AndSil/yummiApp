45f85e4c3ca0178a120e2e1cb3fb834afc0bf325
Iberson <iberson-silva@outlook.com>
2025-04-19 23:19:02 -0500

    fix:solutions in integration to google analitycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-// @ts-ignore
 import mdx from '@astrojs/mdx';
 // https://astro.build/config
 import partytown from '@astrojs/partytown'
-// @ts-ignore
 import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: 'https://astro-article.netlify.app/',
+  site: 'https://yumm-a.netlify.app/',
     integrations: [
         mdx(), 
         sitemap(),
